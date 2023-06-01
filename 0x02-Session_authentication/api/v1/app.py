2f6d1f9647d2764214bed9f4c3d029ea874752e0
Raymond Lukwago <lukwagoraymond3@gmail.com>
2023-06-02 00:47:13 +0300

    Refacted code block

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -57,14 +57,14 @@ def req_filter():
         if state_path is True:
             auth_header = auth.authorization_header(request)
             user = auth.current_user(request)
-            request.current_user = user
             if auth.authorization_header(request) is None\
-                    and auth.session_cookie(request):
+                    and auth.session_cookie(request) is None:
                 abort(401)
             if auth_header is None:
                 abort(401)
             if user is None:
                 abort(403)
+            request.current_user = user
 
 
 if __name__ == "__main__":
