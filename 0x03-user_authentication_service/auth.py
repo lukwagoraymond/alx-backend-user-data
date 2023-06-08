4019d81b82d67c98b552affaec801dfccda76a6a
Raymond Lukwago <lukwagoraymond3@gmail.com>
2023-06-08 20:15:59 +0300

    Corrected Type Annotation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -17,7 +17,7 @@ def _hash_password(password: str) -> bytes:
     return hashed_pwd
 
 
-def _generate_uuid():
+def _generate_uuid() -> str:
     """Generates a random string representation"""
     return str(uuid.uuid4())
 
